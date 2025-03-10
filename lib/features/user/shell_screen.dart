047889101eab9b3b8a2243caf5168bf5b0e3f9ec
Maks Kowalski <71663613+kasrow12@users.noreply.github.com>
2025-03-10 13:32:03 +0100

    Move shell navigation bar to new file

---
+++ lib/features/user/shell_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomerShellScreen extends StatelessWidget {
+  const CustomerShellScreen({required this.navigationShell, super.key});
+
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Główna',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Szukaj',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt),
+            label: 'Moje bilety',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+        currentIndex: navigationShell.currentIndex,
+        onTap: navigationShell.goBranch,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+}
