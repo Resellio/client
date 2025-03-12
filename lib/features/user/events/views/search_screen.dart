1a0bd4fde886f336d7eb0688c928209c8081bb7d
Plebania <mscislowski21@gmail.com>
2025-03-12 17:40:05 +0100

    Added search bar and buttons list

--- lib/features/user/events/views/search_screen.dart
+++ lib/features/user/events/views/search_screen.dart
@@ -16,8 +16,23 @@ class CustomerSearchScreen extends StatelessWidget {
   }
 }
 
-class _CustomerSearchScreen extends StatelessWidget {
+class _CustomerSearchScreen extends StatefulWidget {
   const _CustomerSearchScreen();
+
+  @override
+  _CustomerSearchScreenState createState() => _CustomerSearchScreenState();
+}
+
+class _CustomerSearchScreenState extends State<_CustomerSearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,26 +40,92 @@ class _CustomerSearchScreen extends StatelessWidget {
       create: (_) => EventsCubit()..getEvents(),
       child: BlocBuilder<EventsCubit, EventsState>(
         builder: (context, state) {
-          return switch (state) {
-            EventInitial() => const Center(child: CircularProgressIndicator()),
-            EventsLoading() => const Center(child: CircularProgressIndicator()),
-            //
-            EventsLoaded(:final events) => SingleChildScrollView(
-                child: Column(
-                  children: events
-                      .map(
-                        (event) => EventCard(
-                          event: event,
-                          onTap: () =>
-                              CustomerEventDetailRoute(eventId: event.id)
-                                  .go(context),
-                        ),
-                      )
-                      .toList(),
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.grey,
+                  ),
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: const InputDecoration(
+                      hintText: 'Search events...',
+                      border: InputBorder.none,
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      suffixIcon: Icon(Icons.search),
+                    ),
+                    onChanged: (query) {},
+                  ),
                 ),
               ),
-            EventsError(:final message) => Center(child: Text(message)),
-          };
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: ScrollConfiguration(
+                    behavior: ScrollConfiguration.of(context)
+                        .copyWith(scrollbars: false),
+                    child: SingleChildScrollView(
+                      controller: _scrollController,
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Button 1'),
+                          ),
+                          const SizedBox(width: 8),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Button 2'),
+                          ),
+                          const SizedBox(width: 8),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Button 3'),
+                          ),
+                          const SizedBox(width: 8),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Button 4'),
+                          ),
+                          const SizedBox(width: 8),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Button 5'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )),
+              Expanded(
+                child: switch (state) {
+                  EventInitial() =>
+                    const Center(child: CircularProgressIndicator()),
+                  EventsLoading() =>
+                    const Center(child: CircularProgressIndicator()),
+                  EventsLoaded(:final events) => SingleChildScrollView(
+                      child: Column(
+                        children: events
+                            .map(
+                              (event) => EventCard(
+                                event: event,
+                                onTap: () =>
+                                    CustomerEventDetailRoute(eventId: event.id)
+                                        .go(context),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                  EventsError(:final message) => Center(child: Text(message)),
+                },
+              ),
+            ],
+          );
         },
       ),
     );
